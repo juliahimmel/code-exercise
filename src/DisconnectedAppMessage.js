329f471835645cc7e9f8937cc52c0e088224b07d
Julia Himmel <himmel.julia@gmail.com>
2017-10-19 20:13:36 -0400

    clean up, add more dogs

--- src/DisconnectedAppMessage.js
+++ src/DisconnectedAppMessage.js
@@ -6,7 +6,7 @@ export default () => {
     <h1>Please connect your account to Ethereum to turn your likes into donations. <span>Whenever you &#8220;like&#8221; a pet, a small donation is made to support the pets in our care.</span></h1>
 
     <p className="intro">In order to make that happen, please connect your account to Ethereum. Ethereum offers a highly secure way to make payments without going through banks or credit cards. <a href="#">Click here to learn more about Ethereum</a>.</p>
-    <p><span>Each donation is $0.25.</span></p>
+    <p className="price">Each donation is $0.25.</p>
 
     <p>MetaMask simple way to connect to Ethereum. To enable it in your browser:</p>
     <ul>
--- src/LikedPets.js
+++ src/LikedPets.js
@@ -6,23 +6,23 @@ export default () => {
     <h1>Liked Pets</h1>
     <ul className="pet-list">
       <li className="pet">
-        <img className="pet-image" src="https://images.unsplash.com/photo-1456081445129-830eb8d4bfc6?dpr=1&auto=format&fit=crop&w=1516&q=60&cs=tinysrgb"/>
+        <img className="pet-image" src="https://images.unsplash.com/photo-1456081445129-830eb8d4bfc6"/>
         <div className="description">
           <p className="pet-name">Pudge</p>
           <p className="liked">Liked <span className="status">Pending: Please connect Ethereum</span></p>
         </div>
       </li>
       <li className="pet">
-        <img className="pet-image" src="https://images.unsplash.com/photo-1456081445129-830eb8d4bfc6?dpr=1&auto=format&fit=crop&w=1516&q=60&cs=tinysrgb"/>
+        <img className="pet-image" src="https://images.unsplash.com/photo-1502673530728-f79b4cab31b1?w=1350"/>
         <div className="description">
-          <p className="pet-name">Pudge</p>
+          <p className="pet-name">Loki</p>
           <p className="liked">Liked <span className="status">Pending: Please connect Ethereum</span></p>
         </div>
       </li>
       <li className="pet">
-        <img className="pet-image" src="https://images.unsplash.com/photo-1456081445129-830eb8d4bfc6?dpr=1&auto=format&fit=crop&w=1516&q=60&cs=tinysrgb"/>
+        <img className="pet-image" src="https://images.unsplash.com/photo-1474088122367-1c381a17db5c"/>
         <div className="description">
-          <p className="pet-name">Pudge</p>
+          <p className="pet-name">Hazel</p>
           <p className="liked">Liked <span className="status">Pending: Please connect Ethereum</span></p>
         </div>
       </li>
